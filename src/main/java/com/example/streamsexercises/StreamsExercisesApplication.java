538733e3769af30ea8b0bc89d2b45a3afe4bee23
pvadla <praveen454griet@gmail.com>
2023-12-19 23:12:49 +0530

    reactive

---
+++ src/main/java/com/example/streamsexercises/StreamsExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.example.streamsexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamsExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamsExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/streamsexercises/StreamsExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.streamsexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
